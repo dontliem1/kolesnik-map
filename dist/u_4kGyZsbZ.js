4f81408e4ae124760ee7c5881cfeaf9eed985d3d
dontliem1 <dontliem1@users.noreply.github.com>
2025-11-15 18:19:46 +0000

    deploy: e785a5c9272fa3b1028be4b5d74b4b241f518e50

--- dist/u_4kGyZsbZ.js
+++ dist/u_4kGyZsbZ.js
@@ -1,4 +1,4 @@
-import {createProjectMap} from '/lib/ymaps.js'
+import {createProjectMap} from '/kolesnik-map/lib/ymaps.js'
 
 	const map = createProjectMap('65.544299', '57.157915', 'Водопроводная, 14');
 	console.log(map);
