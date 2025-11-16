4ec2760694f687ddca3ce252b59431e8749ffeea
m1 <dontliem1@gmail.com>
2025-11-16 07:09:12 +0500

    favicon and font

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 export default {
 	title: "Eleventy Base Blog v9",
 	url: "https://example.com/",
-	language: "en",
+	language: "ru",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
